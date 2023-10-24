f9c9f0c619671561837420223b2c6602a56d3dea
Vidhi Sahai <72317428+Vidhi2604@users.noreply.github.com>
2023-10-24 18:00:31 +0530

    Add files via upload

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,10 @@
+import testimonial from './testimonial'
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [testimonial, abouts, brands, contact, experiences, skills, workExperience, works]
