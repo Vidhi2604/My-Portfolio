2957168c7c83a2650f8a51996b11fb8aace05f33
Vidhi Sahai <72317428+Vidhi2604@users.noreply.github.com>
2023-12-18 20:15:59 +0530

    Add files via upload

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,81 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import api from '../assets/api.png';
+import cpp from '../assets/cpp.png';
+import c from '../assets/c.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import canva from '../assets/canva.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import sass from '../assets/sass.png';
+import latex from '../assets/latex.png';
+import msword from '../assets/msword.png';
+import sanity from '../assets/sanity.png';
+import tinkercad from '../assets/tinkercad.png';
+import vscode from '../assets/vscode.png';
+import linkedin from '../assets/linkedin.png';
+import instagram from '../assets/instagram.png';
+import facebook from '../assets/facebook.png';
+import github from '../assets/github.png';
+import twitter from '../assets/twitter.png';
+
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+import project01 from '../assets/project01.png';
+import project02 from '../assets/project02.png';
+
+import profile from '../assets/profile.png';
+import framermotion from '../assets/framermotion.svg';
+import circle from '../assets/circle.svg';
+import logo from '../assets/logo.png';
+import bg from '../assets/bg.png';
+import ME from '../assets/ME.png';
+
+
+export default {
+  email,
+  mobile,
+  api,
+  c,
+  cpp,
+  css,
+  figma,
+  canva,
+  git,
+  html,
+  javascript,
+  node,
+  python,
+  react,
+  sass,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
+  logo,
+  bg,
+  ME,
+  vscode,
+  tinkercad,
+  sanity,
+  msword,
+  latex,
+  framermotion,
+  linkedin,
+  instagram,
+  facebook,
+  github,
+  twitter,
+  project01,
+  project02
+};
